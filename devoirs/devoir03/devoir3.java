51d290359a1740e5f1c991ad245fa316b588ff5f
Fanny Riedo <141828170+riedo-emf@users.noreply.github.com>
2024-11-13 22:33:08 +0100

    Pas mal mais il y a un petit problème

--- devoirs/devoir03/devoir3.java
+++ devoirs/devoir03/devoir3.java
@@ -9,15 +9,16 @@ public class devoir3 {
 
    int[] jours = new int[]{31,28,31,30,31,30,31,31,30,31,30,31};
     
-        
+        //RIF: Attention, en janvier, le nombre de jour est dans la cellule 0, et le numéro du mois est 1. 
+        //RIF: attention également, vous devez savoir le faire avec des tests if ou switch (la donnée limitait les instructions disponibles)
     for (int i = 0; i < mois -1; i++) {
         joursan = joursan + jours[i];
     }
     joursan += jour;
-    if (annee % 4 == 0 && mois > 2) {
+    if (annee % 4 == 0 && mois > 2) { //RIF: et les années spéciales? (multiples de 100 et de 400 :-) )
         joursan = joursan + 1;
     }
-System.out.println(joursan);
+System.out.println(joursan); //RIF: ajoutez une petite phrase là autour
 
 
 
